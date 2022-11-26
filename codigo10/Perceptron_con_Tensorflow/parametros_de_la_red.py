3afca05dfe433570a43073041e13385f98b353fe
lorenzoo22 <lagudmarr@gmail.com>
2022-11-26 11:38:24 +0100

    Solucionando el placeholder y demas

--- codigo10/Perceptron_con_Tensorflow/parametros_de_la_red.py
+++ codigo10/Perceptron_con_Tensorflow/parametros_de_la_red.py
@@ -8,18 +8,20 @@ valores_a_predecir_Y = [[0.], [1.], [0.], [0.]]
 #-------------------------------------
 import tensorflow as tf
 
+#Esta sentencia posibilita la ejecución de la red neuronal
+tf.compat.v1.disable_eager_execution()
 
 #Variable TensorFLow correspondiente a los valores de neuronas de entrada
-tf_neuronas_entradas_X = tf.placeholder(tf.float32, [None, 2])
+tf_neuronas_entradas_X = tf.compat.v1.placeholder(tf.float32, [None, 2])
 
 #Variable TensorFlow correspondiente a la neurona de salida (predicción real)
-tf_valores_reales_Y = tf.placeholder(tf.float32, [None, 1])
+tf_valores_reales_Y = tf.compat.v1.placeholder(tf.float32, [None, 1])
 
 #-- Peso --
 #Creación de una variable TensorFlow de tipo tabla
 #que contiene 2 entradas y cada una tiene un peso [2,1]
 #Estos valores se inicializan al azar
-peso = tf.Variable(tf.random_normal([2, 1]), tf.float32)
+peso = tf.Variable(tf.compat.v1.random_normal([2, 1]), tf.float32)
 
 #-- Sesgo inicializado a 0 --
 sesgo = tf.Variable(tf.zeros([1, 1]), tf.float32)
@@ -39,7 +41,7 @@ prediccion = tf.sigmoid(sumaponderada)
 funcion_error = tf.reduce_sum(tf.pow(tf_valores_reales_Y-prediccion,2))
 
 #Descenso de gradiente con una tasa de aprendizaje fijada a 0,1
-optimizador = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(funcion_error)
+optimizador = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.1).minimize(funcion_error)
 
 
 
